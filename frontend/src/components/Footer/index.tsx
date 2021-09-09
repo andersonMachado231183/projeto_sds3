8f0908d8f105c1faa66ff20629cd03e1704dcb33
andersonMachado231183 <pitt231183@gmail.com>
2021-09-09 20:35:50 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/andersonMachado231183" target="_blank" rel="noreferrer">Anderson Arnold Machado</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        </div>
+      </footer>
+      
+      
+    );
+}
+
+export default index;
